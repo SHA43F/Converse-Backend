5a9243085af7f7b4851952d47d30010d58e1d063
SHA43F <shareefpmk44@gmail.com>
2023-01-28 21:03:49 +0530

    4.3) Cors & Integration

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import sequelize from "./database/sqlDatabase.js";
 import signUpRouter from "./routes/signUpRouter.js";
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
